dfb3c4cf7692a24c1136159ba18cb514ded6be43
Joelma Camargo <joelmavilela023@gmail.com>
2023-08-30 16:29:57 -0300

    Estrutura else-if

---
+++ estrutura-else-if/else-if.js
@@ -0,0 +1,15 @@
+idade = 13
+
+if(idade >= 4 && idade < 6){
+    console.log("Pré-escola");
+}
+else if(idade >= 6 && idade < 15){
+    console.log("Ensino fundamental");
+}
+else if(idade >= 15 && idade < 18){
+    console.log("Ensino médio");
+}
+else{
+    console.log("Aluno está fora do padrão escolar");
+}
+console.log("FIM")
